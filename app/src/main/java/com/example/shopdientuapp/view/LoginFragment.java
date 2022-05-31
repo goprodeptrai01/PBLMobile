19a2731d8190013b6903463496e4b741b1621824
mydeptrai2107 <80403942+mydeptrai2107@users.noreply.github.com>
2022-06-01 00:51:06 +0700

    commit giao dien login

---
+++ app/src/main/java/com/example/shopdientuapp/view/LoginFragment.java
@@ -0,0 +1,30 @@
+package com.example.shopdientuapp.view;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.example.shopdientuapp.R;
+
+public class LoginFragment extends Fragment {
+
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        if (getArguments() != null) {
+
+        }
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+
+        return inflater.inflate(R.layout.fragment_login, container, false);
+    }
+}
