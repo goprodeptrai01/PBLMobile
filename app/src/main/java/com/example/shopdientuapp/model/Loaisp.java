e701851b094fc633ee427b0bdbf2e5bbfe375975
mydeptrai2107 <80403942+mydeptrai2107@users.noreply.github.com>
2022-06-01 16:24:44 +0700

    commit giao dien home, san pham new va list san pham

---
+++ app/src/main/java/com/example/shopdientuapp/model/Loaisp.java
@@ -0,0 +1,39 @@
+package com.example.shopdientuapp.model;
+
+import java.io.Serializable;
+
+public class Loaisp implements Serializable {
+    private int Id;
+    private String Tenloaisp;
+    private String Hinhanhloaisp;
+
+    public Loaisp(int id, String tenloaisp, String hinhanhloaisp) {
+        Id = id;
+        Tenloaisp = tenloaisp;
+        Hinhanhloaisp = hinhanhloaisp;
+    }
+
+    public int getId() {
+        return Id;
+    }
+
+    public void setId(int id) {
+        Id = id;
+    }
+
+    public String getTenloaisp() {
+        return Tenloaisp;
+    }
+
+    public void setTenloaisp(String tenloaisp) {
+        Tenloaisp = tenloaisp;
+    }
+
+    public String getHinhanhloaisp() {
+        return Hinhanhloaisp;
+    }
+
+    public void setHinhanhloaisp(String hinhanhloaisp) {
+        Hinhanhloaisp = hinhanhloaisp;
+    }
+}
--- app/src/main/java/com/example/shopdientuapp/view/LoginFragment.java
+++ app/src/main/java/com/example/shopdientuapp/view/LoginFragment.java
@@ -2,16 +2,22 @@ package com.example.shopdientuapp.view;
 
 import android.os.Bundle;
 
+import androidx.annotation.NavigationRes;
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.navigation.Navigation;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
 import com.example.shopdientuapp.R;
+import com.example.shopdientuapp.databinding.FragmentHomeBinding;
+import com.example.shopdientuapp.databinding.FragmentLoginBinding;
 
 public class LoginFragment extends Fragment {
-
+    private FragmentLoginBinding binding;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -24,7 +30,20 @@ public class LoginFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
+        binding = FragmentLoginBinding.inflate(inflater, container, false);
+        return binding.getRoot();
+    }
 
-        return inflater.inflate(R.layout.fragment_login, container, false);
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
+        binding.btnLogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Bundle bundle = new Bundle();
+                Navigation.findNavController(binding.btnLogin).navigate(R.id.homeFragment);
+            }
+        });
     }
 }
