c4dd682698a81b8988b6c84283c278ca906325b9
Cao Minh Tri <caominhtri112201@gmail.com>
2022-06-02 12:35:47 +0700

    add Login

---
+++ app/src/main/java/com/example/shopdientuapp/prevalent/Prevalent.java
@@ -0,0 +1,7 @@
+package com.example.shopdientuapp.prevalent;
+
+import com.example.shopdientuapp.model.Users;
+
+public class Prevalent {
+    private static Users currentOnlineUser;
+}
--- app/src/main/java/com/example/shopdientuapp/view/LoginFragment.java
+++ app/src/main/java/com/example/shopdientuapp/view/LoginFragment.java
@@ -1,5 +1,6 @@
 package com.example.shopdientuapp.view;
 
+import android.app.ProgressDialog;
 import android.os.Bundle;
 
 import androidx.annotation.NavigationRes;
@@ -8,16 +9,27 @@ import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 import androidx.navigation.Navigation;
 
+import android.text.TextUtils;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Toast;
 
 import com.example.shopdientuapp.R;
 import com.example.shopdientuapp.databinding.FragmentHomeBinding;
 import com.example.shopdientuapp.databinding.FragmentLoginBinding;
+import com.example.shopdientuapp.model.Users;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
 public class LoginFragment extends Fragment {
+
     private FragmentLoginBinding binding;
+    private ProgressDialog loadingBar;
+    private String parentDbName = "Users";
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -38,11 +50,69 @@ public class LoginFragment extends Fragment {
     public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
 
+        loadingBar = new ProgressDialog(getActivity());
+
         binding.btnLogin.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Bundle bundle = new Bundle();
-                Navigation.findNavController(binding.btnLogin).navigate(R.id.homeFragment);
+//                Bundle bundle = new Bundle();
+//                Navigation.findNavController(binding.btnLogin).navigate(R.id.homeFragment);
+                loginUser();
+            }
+        });
+    }
+
+    private void loginUser() {
+        String phone = binding.edtUser.getText().toString();
+        String password = binding.edtPass.getText().toString();
+
+        if (TextUtils.isEmpty(phone)) {
+            Toast.makeText(getActivity(), "Please enter your phone number...", Toast.LENGTH_SHORT).show();
+        }
+        else if (TextUtils.isEmpty(password)) {
+            Toast.makeText(getActivity(), "Please enter your password...", Toast.LENGTH_SHORT).show();
+        }
+        else {
+            loadingBar.setTitle("Login Account");
+            loadingBar.setMessage("Please wait,while we are checking the credentials.");
+            loadingBar.setCanceledOnTouchOutside(false);
+            loadingBar.show();
+
+            AllowAccessToAccount(phone, password);
+        }
+    }
+
+    private void AllowAccessToAccount(String phone, String password) {
+        final DatabaseReference RootRef;
+        RootRef = FirebaseDatabase.getInstance().getReference();
+
+
+        RootRef.addListenerForSingleValueEvent(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot snapshot) {
+                if (snapshot.child(parentDbName).child(phone).exists()) {
+                    Users usersdata = snapshot.child(parentDbName).child(phone).getValue(Users.class);
+
+                    if (usersdata.getPhone().equals(phone) && usersdata.getPassword().equals(password)) {
+                        Toast.makeText(getActivity(), "Login Successfully...", Toast.LENGTH_SHORT).show();
+                        loadingBar.dismiss();
+
+                        Navigation.findNavController(binding.btnLogin).navigate(R.id.homeFragment);
+                    }
+                    else {
+                        Toast.makeText(getActivity(), "Your password maybe incorrect.Please try again.", Toast.LENGTH_SHORT).show();
+                        loadingBar.dismiss();
+                    }
+                }
+                else {
+                    Toast.makeText(getActivity(), "Account with this " + phone + "number do not exists ", Toast.LENGTH_SHORT).show();
+                    loadingBar.dismiss();
+                }
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+
             }
         });
     }
