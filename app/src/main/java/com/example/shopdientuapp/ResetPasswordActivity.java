e2bfbd150cf1751318754aa6e907c7f8a8ac8fd3
goprodeptra01 <tapnguyen01@gmail.com>
2022-06-06 21:10:37 +0700

    tao cac activity

--- app/src/main/java/com/example/shopdientuapp/ResetPasswordActivity.java
+++ app/src/main/java/com/example/shopdientuapp/ResetPasswordActivity.java
@@ -4,11 +4,11 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
-public class ConfirmFinalOrderActivity extends AppCompatActivity {
+public class ResetPasswordActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_confirm_final_order);
+        setContentView(R.layout.activity_reset_password);
     }
 }
